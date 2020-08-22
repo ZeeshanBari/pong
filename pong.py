4e65930a7d9ae6ba6493e6fcdd20cd719759d3d5
ZeeshanBari <zeeshan.bari13@gmail.com>
2020-08-22 13:05:10 -0400

    pong game finished

---
+++ pong.py
@@ -0,0 +1,140 @@
+import turtle
+import os
+
+wn = turtle.Screen()
+wn.title('Pong')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)  # +-400, +-300
+wn.tracer(0)  # Stops the window from updating - so goes faster
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)  # speed of animation = max
+paddle_a.shape('square')  # circle, square, triangle
+paddle_a.color('white')
+# by default 20 pixels by 20 pixels
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()  # by default draws a line
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)  # speed of animation = max
+paddle_b.shape('square')  # circle, square, triangle
+paddle_b.color('white')
+# by default 20 pixels by 20 pixels
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()  # by default draws a line
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)  # speed of animation = max
+ball.shape('square')  # circle, square, triangle
+ball.color('white')
+# by default 20 pixels by 20 pixels
+ball.penup()  # by default draws a line
+ball.goto(0, 0)
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 200)
+pen.write('Player A: 0 \nPlayer B: 0', align='center',
+          font=('Courier', 24, 'normal'))
+
+
+# Function
+
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    if paddle_a.ycor() + 50 < 300:
+        y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    if paddle_a.ycor() - 50 > -280:
+        y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    if paddle_b.ycor() + 50 < 300:
+        y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    if paddle_b.ycor() - 50 > -280:
+        y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, 'w')
+wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
+ball.dx = 2
+ball.dy = -2
+
+
+# Main Game Loop
+while True:
+    wn.update()  # updates the screen every time it runs
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system('afplay bounce.wav&')
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        os.system('afplay bounce.wav&')
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write('Player A: {} \nPlayer B: {}'.format(score_a, score_b), align='center',
+                  font=('Courier', 24, 'normal'))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write('Player A: {} \nPlayer B: {}'.format(score_a, score_b), align='center',
+                  font=('Courier', 24, 'normal'))
+
+    # Paddle Ball Collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        os.system('afplay bounce.wav&')
+
+    # Paddle Ball Collisions
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        os.system('afplay bounce.wav&')
